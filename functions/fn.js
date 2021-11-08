0d1175c1f5219870060c9bae33619d2ad0a29234
MaksymKostariev <kostarev9@gmail.com>
2021-11-08 13:01:56 +0200

    new folders

---
+++ functions/fn.js
@@ -0,0 +1,24 @@
+function square(a) {
+    return a * a;
+}
+console.log("Square is equel ", square(8));
+
+function squareAndOne(a) {
+    let s = a * a;
+    return s += 1;
+}
+console.log("Square plus One is equel", squareAndOne(8));
+
+const sqr = (a) => {
+    return a * a;
+}
+console.log("Arrow function is ", sqr(4));
+
+const sqr2 = (a) => a * a;
+console.log("Arrow function2 is ", sqr2(4));
+
+const sqrt = function (a){
+    return a * a;
+}
+console.log("Function expression is ", sqrt(3));
+
