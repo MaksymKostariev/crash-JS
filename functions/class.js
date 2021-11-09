77809e60c21182a2bd71f556a3b2a813bcc82f44
MaksymKostariev <kostarev9@gmail.com>
2021-11-09 10:42:16 +0200

    added functions

--- functions/class.js
+++ functions/class.js
@@ -10,22 +10,35 @@ class motorcycle {
 }  
 
 const GS = new motorcycle("BMW", "R 1250 GS");
-const Hornet = new motorcycle("Honda", "CB 600 F")
+
+function newBike(obj){
+    return Object.create(obj);;
+}
+
+const Hornet = newBike(GS);
+
+Hornet.brand = "Honda";
+Hornet.model = "CB 600 F";
 
 console.log("GS is", GS.bike());
 console.log("Hornet is", Hornet.bike());
 console.log("");
 
-GS.class = "enduro";
-Hornet.class = "street";
+function newField(obj, key, value) {
+   obj[key] = value;
+   return obj;
+}
+
+newField(GS, "class", "enduro");
+newField(Hornet, "class", "street");
 
 function testRide(bike, type) {
     console.log("Your bike is", bike);
 
     if (type === "enduro") {
-        console.log("Your going OFF Road trip");
+        console.log("You'r going OFF Road trip");
     } else if (type === "street") {
-        console.log("Your going Road trip");
+        console.log("You'r going Road trip");
     }
 }
 
